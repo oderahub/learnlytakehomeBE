9a744bd42fd459819b6323e81e401d6d2ba468e8
oderahub <okere_chidera@yahoo.com>
2024-08-07 16:47:41 +0100

    fix: comment and log

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://learnly-vue--wheat.vercel.app', 
+    origin: '*', 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: [
       "Origin",
